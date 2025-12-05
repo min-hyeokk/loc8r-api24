c2a65e180359ca93db1f75d4bed2722e1d9d1cdf
min-hyeokk <166090431+min-hyeokk@users.noreply.github.com>
2025-12-05 17:37:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(passport.initialize());
 app.use('/api', apiRouter);
 app.use('/users', usersRouter);
 
-app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function(req, res, next) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
 });
 
